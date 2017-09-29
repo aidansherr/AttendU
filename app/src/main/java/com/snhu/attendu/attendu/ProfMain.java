22bbf7e7b19678b54081795d7bf9408511a30b9c
amccracken <aidan.sherr@snhu.edu>
2017-09-29 15:07:48 -0400

    Added Professor Landing Page- Update

--- app/src/main/java/com/snhu/attendu/attendu/ProfMain.java
+++ app/src/main/java/com/snhu/attendu/attendu/ProfMain.java
@@ -3,11 +3,11 @@ package com.snhu.attendu.attendu;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Professor_Main extends AppCompatActivity {
+public class ProfMain extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_professor__main);
+        setContentView(R.layout.activity_prof_main);
     }
 }
