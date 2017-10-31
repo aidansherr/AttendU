7b0b642c327e949109e3ccf8788b0fe194190ac0
Tyler <towens182>
2017-10-31 15:08:38 -0400

    Merge

--- app/src/main/java/com/snhu/attendu/attendu/PasswordDigest.java
+++ app/src/main/java/com/snhu/attendu/attendu/PasswordDigest.java
@@ -1,8 +1,43 @@
 package com.snhu.attendu.attendu;
 
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Formatter;
+import static android.R.id.input;
+
 /**
  * Created by tyowe on 10/24/2017.
  */
 
-public class PasswordDigest {
+public class PasswordDigest
+{
+    public static String encryptPassword(String password)
+    {
+        String securePass = "";
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-1");
+            digest.reset();
+            digest.update(password.getBytes("UTF-8"));
+            securePass = byteToHex(digest.digest());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return securePass;
+    }
+
+    private static String byteToHex(final byte[] hash)
+    {
+        Formatter formatter = new Formatter();
+        for (byte b : hash)
+        {
+            formatter.format("%02x", b);
+        }
+        String result = formatter.toString();
+        formatter.close();
+        return result;
+    }
 }
