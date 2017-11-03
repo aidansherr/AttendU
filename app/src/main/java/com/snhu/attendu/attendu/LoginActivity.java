9dc2b6365b652696b16f4c510aff8815f094aa00
Tyler <towens182>
2017-11-03 10:53:38 -0400

    Merge

--- app/src/main/java/com/snhu/attendu/attendu/LoginActivity.java
+++ app/src/main/java/com/snhu/attendu/attendu/LoginActivity.java
@@ -5,7 +5,9 @@ import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -318,10 +320,12 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         private final String mEmail;
         private final String mPassword;
-        private final String newUser;
+        private String newUser;
         private String filePath = "userData.txt";
+        private SharedPreferences sharedPreferences;
+        private String PREF_NAME = "prefs";
 
-        UserLoginTask(String email, String password) {
+        UserLoginTask(String email, String password) { //Add string userlevel
             mEmail = email;
             mPassword = password;
             //Fields
@@ -359,24 +363,26 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
 
             }
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+            String credential = preferences.getString("user", "");
+            String[] pieces = credential.split(":");
+            if (pieces[0].equals(mEmail)) {
+                // Account exists, return true if the password matches.
+                PasswordDigest pd = new PasswordDigest();
+                return pieces[1].equals(pd.encryptPassword(mPassword));
+            }
 
             //DEBUG to find out encrypted password
             Log.d("TEST", "The entered password: " + mPassword);
 
             // TODO: register the new account here.
             //Consider something else
-            try{
-                UserLevel = "s";
-                PasswordDigest pd = new PasswordDigest();
-                pd.encryptPassword(mPassword);
-                BufferedWriter writer = new BufferedWriter(new FileWriter(filePath));
-                writer.write(newUser);
-                writer.close();
-
-
-            } catch (IOException e){
-                System.err.format("IOException: %s%n", e);
-            }
+            PasswordDigest pd = new PasswordDigest();
+            newUser = (mEmail + ":" + pd.encryptPassword(mPassword) + ":s" + "/n");
+           // SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putString("user", newUser);
+            editor.apply();
 //            try{
 //                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("testUsers.txt", Context.MODE_PRIVATE));
 //                outputStreamWriter.write((mEmail + ":" + mPassword + "/n"));
