e51d6f23f8982afd4b7356f814e21b8801001cc4
Eddie <edward.greer@snhu.edu>
2017-11-16 20:42:21 -0500

    Added user pages for specific level as well as identifying userlevel on login

--- app/src/main/java/com/snhu/attendu/attendu/Course.java
+++ app/src/main/java/com/snhu/attendu/attendu/Course.java
@@ -5,32 +5,49 @@ package com.snhu.attendu.attendu;
  * Created by Travis on 10/11/2017.
  */
 //This class is to represent a physical class that a student would be taking
-public class Course
-{
-    public
-    Course(String className)
-    {
-        this.className=className;
+public class Course {
+    public Course(String className) {
+        this.className = className;
     }
-    String getClassName()
-    {
+
+    String getClassName() {
         return className;
     }
-    void setClassName(String newName)
-    {
-        className=newName;
+
+    void setClassName(String newName) {
+        className = newName;
     }
-    void setCourseAvailability(boolean available)
-    {
+
+    void setCourseAvailability(boolean available) {
         courseCheckinAvailable = available;
     }
-    boolean getCourseAvailibility()
-    {
+
+    boolean getCourseAvailibility() {
         return courseCheckinAvailable;
     }
 
     private
 
-    String className="";
+    String className = "";
     boolean courseCheckinAvailable = false;
+
+public void checkPin()
+    {
+
+        SignInPin Pin = new SignInPin();
+        String random = Integer.toString(Pin.GenerateCode());
+        String userPinInput = "0";//change based on user input from pinbox
+
+        boolean CodeCheck = false;
+        if(random.equals(userPinInput))
+        {
+            CodeCheck = true;
+        }
+    else
+        {
+            //
+        }
+    }
+
+
 }
