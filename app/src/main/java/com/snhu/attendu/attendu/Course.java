ebfd25962803383a410fc119607987a15c830801
travis.guthrie1 <tjg0512@gmail.com>
2018-02-08 13:02:54 -0500

    Database work

--- app/src/main/java/com/snhu/attendu/attendu/Course.java
+++ app/src/main/java/com/snhu/attendu/attendu/Course.java
@@ -1,16 +1,24 @@
 package com.snhu.attendu.attendu;
 
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 /**
  * Created by Travis on 10/11/2017.
  */
 //This class is to represent a physical class that a student would be taking
 public class Course
 {
+
     public
     SignInPin pin = new SignInPin();
     String random = "12345";
     //String random = Integer.toString(pin.GenerateCode());
+    public Course()
+    {
+        addCourse();
+    }
     Course(String className)
     {
         this.className=className;
@@ -30,6 +38,14 @@ public class Course
     boolean getCourseAvailibility()
     {
         return courseCheckinAvailable;
+    }
+    void addCourse()
+    {
+        FirebaseDatabase mDatabase;
+        mDatabase= FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference=mDatabase.getReference();
+        databaseReference.child("Course").push().setValue(this);
+
     }
 
     private
