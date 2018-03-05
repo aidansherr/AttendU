4837f5097debbc4060f170ba34fb38a1640e5026
Travis Guthrie <tjg0512@gmail.com>
2018-03-05 12:57:59 -0500

    Professor and Student now pull all of their data to the database.

--- app/src/main/java/com/snhu/attendu/attendu/StudentMain.java
+++ app/src/main/java/com/snhu/attendu/attendu/StudentMain.java
@@ -9,33 +9,49 @@ import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class StudentMain extends AppCompatActivity {
 
 
-    List<Course> courses=new ArrayList<Course>();
+
     Student newUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+        FirebaseDatabase mDatabase;
+        mDatabase= FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference=FirebaseDatabase.getInstance().getReference();
 
-        Course math=new Course("Math");
-        Course english=new Course("English");
-        Course cs= new Course("CS");
 
-        courses.add(math);
-        courses.add(english);
-        courses.add(cs);
 
-        courses.get(1).setCourseAvailability(true);
 
-        newUser= new Student("Tyler",courses,"P");
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_main);
-        makeButtons();
+        //This pulls the student object from the database and makes buttons based on the classes it possese
+        databaseReference.child("Student").child("Student_ID").child("-L6r1wPEIM_7FxA7CFDT").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Student value = dataSnapshot.getValue(Student.class);
+
+                newUser=new Student(value.getUserName(),value.getClassList(),value.getUser());
+                makeButtons(newUser);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
 
 
     }
@@ -45,14 +61,14 @@ public class StudentMain extends AppCompatActivity {
         startActivity(inten);
     }
 
-    public void makeButtons()
+    public void makeButtons(Student newUser)
     {
-        for(int i=0;i<courses.size();i++)
+        for(int i=0;i<newUser.getClassList().size();i++)
         {
             TableRow row = new TableRow(this);
             TableLayout tableLayout = (TableLayout)findViewById(R.id.tableLayout);
             Button btn= new Button(this);
-            btn.setText(newUser.getClassName(courses.get(i)));
+            btn.setText(newUser.getClassName(newUser.getClassList().get(i)));
             btn.setHeight(150);
             btn.setWidth(300);
             btn.setOnClickListener(new View.OnClickListener()
@@ -67,7 +83,7 @@ public class StudentMain extends AppCompatActivity {
             tableLayout.addView(row);
             row.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT,TableLayout.LayoutParams.MATCH_PARENT,1.0f));
             row.addView(btn);
-            if (courses.get(i).getCourseAvailibility() == true)
+            if (newUser.getClassList().get(i).getCourseAvailibility() == true)
             {
                 btn.setBackgroundColor(Color.GREEN);
             }
