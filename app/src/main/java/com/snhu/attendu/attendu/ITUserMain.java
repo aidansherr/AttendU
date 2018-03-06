73d1f0c3e533cfe9a548a2e2a27ce68b790f072a
Travis Guthrie <tjg0512@gmail.com>
2018-03-06 18:42:20 -0500

    IT user and the Admin user now pull from the database

--- app/src/main/java/com/snhu/attendu/attendu/ITUserMain.java
+++ app/src/main/java/com/snhu/attendu/attendu/ITUserMain.java
@@ -6,13 +6,44 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class ITUserMain extends AppCompatActivity {
 
     private Button mCreateButton;
+    final ITUser newUser=new ITUser();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
         super.onCreate(savedInstanceState);
+
+        FirebaseDatabase mDatabase;
+        mDatabase= FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference=FirebaseDatabase.getInstance().getReference();
+
+
+        databaseReference.child("IT_user").child("IT_User_ID").child("-L6xb8CWAoPQIk1mc9ee").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                ITUser value = dataSnapshot.getValue(ITUser.class);
+                newUser.setUserName(value.getUserName());
+                newUser.setUserType(value.getUser());
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
         setContentView(R.layout.activity_ituser_main);
 
         mCreateButton = (Button) findViewById(R.id.createUserButton);
