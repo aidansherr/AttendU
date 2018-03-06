5b93fe09e469749dac832d931136c7145460b59b
Travis Guthrie <tjg0512@gmail.com>
2018-03-06 17:24:12 -0500

    I merged the databaseWork branch with master and professor and Student now get their data from the database

--- app/src/main/java/com/snhu/attendu/attendu/StudentMain.java
+++ app/src/main/java/com/snhu/attendu/attendu/StudentMain.java
@@ -4,45 +4,54 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
+import com.google.firebase.database.ValueEventListener;
 
 public class StudentMain extends AppCompatActivity {
 
+    private View mLayout;
+    private TextView mStudentLabel;
 
 
     Student newUser;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         FirebaseDatabase mDatabase;
         mDatabase= FirebaseDatabase.getInstance();
-        DatabaseReference databaseReference=FirebaseDatabase.getInstance().getReference();
-
-
-
-
+        DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReference();
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_main);
-        //This pulls the student object from the database and makes buttons based on the classes it possese
+
+        mLayout = findViewById(R.id.student_linear);
+        mStudentLabel = (TextView) findViewById(R.id.student_label);
+
+        //TODO Read courses from database here and student
+
         databaseReference.child("Student").child("Student_ID").child("-L6r1wPEIM_7FxA7CFDT").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 Student value = dataSnapshot.getValue(Student.class);
 
                 newUser=new Student(value.getUserName(),value.getClassList(),value.getUser());
+                mStudentLabel.setText(newUser.getUserName());
                 makeButtons(newUser);
             }
 
@@ -54,23 +63,59 @@ public class StudentMain extends AppCompatActivity {
 
 
 
+
     }
+
     public void openPinWindow(View view)
     {
         Intent inten= new Intent(this,CodeCheck.class);
         startActivity(inten);
     }
 
+    private void reportAbsence(int num)
+    {
+        //TODO add parameters to new intent, reference to class
+        String courseAbName = newUser.getClassList().get(num).getClassName();
+
+        Intent absenceIntent = new Intent(getApplicationContext(), Absence.class);
+        absenceIntent.putExtra("courseName",courseAbName);
+        startActivity(absenceIntent);
+    }
+
+
     public void makeButtons(Student newUser)
     {
-        for(int i=0;i<newUser.getClassList().size();i++)
+       LinearLayout mParentLayout = (LinearLayout) findViewById(R.id.student_linear);
+        List<Course> courses=newUser.getClassList();
+        for(int i=0;i<courses.size();i++)
         {
-            TableRow row = new TableRow(this);
-            TableLayout tableLayout = (TableLayout)findViewById(R.id.tableLayout);
+            LinearLayout dualView = new LinearLayout(getApplicationContext());
+            mParentLayout.addView(dualView);
+            dualView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            dualView.setOrientation(LinearLayout.HORIZONTAL);
+
             Button btn= new Button(this);
-            btn.setText(newUser.getClassName(newUser.getClassList().get(i)));
-            btn.setHeight(150);
-            btn.setWidth(300);
+            final Button absenceBtn = new Button (this);
+            LinearLayout.LayoutParams buttonParams =
+                    new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.FILL_PARENT);
+            LinearLayout.LayoutParams aButtonParams =
+                    new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.FILL_PARENT);
+            buttonParams.weight = 1;
+            aButtonParams.weight = 3;
+            btn.setLayoutParams(buttonParams);
+            absenceBtn.setLayoutParams(aButtonParams);
+            buttonParams.setMargins(0,60,0,0);
+            aButtonParams.setMargins(0,60,0,0);
+            btn.setId(i);
+            absenceBtn.setId(i);
+            btn.setText(newUser.getClassName(courses.get(i)));
+
+            absenceBtn.setText(R.string.report_absence);
+            absenceBtn.setBackgroundColor(Color.LTGRAY);
+
+            dualView.addView(btn);
+            dualView.addView(absenceBtn);
+
             btn.setOnClickListener(new View.OnClickListener()
             {
                 @Override
@@ -78,18 +123,23 @@ public class StudentMain extends AppCompatActivity {
                 {
                     openPinWindow(view);
                 }
-
             });
-            tableLayout.addView(row);
-            row.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT,TableLayout.LayoutParams.MATCH_PARENT,1.0f));
-            row.addView(btn);
-            if (newUser.getClassList().get(i).getCourseAvailibility() == true)
+
+            absenceBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    reportAbsence(absenceBtn.getId());
+                }
+            });
+
+            if (courses.get(i).getCourseAvailibility() == true)
             {
-                btn.setBackgroundColor(Color.GREEN);
+                btn.setBackgroundColor(Color.rgb(50, 205, 50)); //GREEN
+            }else
+            {
+                btn.setBackgroundColor(Color.rgb(220, 220 ,220)); //GRAY
             }
         }
     }
-
-
-
 }
