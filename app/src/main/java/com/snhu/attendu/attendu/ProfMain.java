d1274a544912744c5d2a148150fb16d5023a13a1
Tyler <tyowens182@gmail.com>
2018-03-07 16:01:34 -0500

    Professor Design added

--- app/src/main/java/com/snhu/attendu/attendu/ProfMain.java
+++ app/src/main/java/com/snhu/attendu/attendu/ProfMain.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.TableRow;
@@ -20,30 +21,38 @@ import static android.R.interpolator.linear;
 public class
 ProfMain extends AppCompatActivity
 {
+    private View mLayout;
+    private TextView mProfLabel;
+
     List<Course> courses= new ArrayList<Course>();
     Professor newUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
-
     {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prof_main);
 
+        mLayout = findViewById(R.id.prof_linear);
+        mProfLabel = (TextView) findViewById(R.id.prof_label);
 
+        //TODO load course from database
         Course math=new Course("Math");
         Course english=new Course("English");
         Course cs= new Course("Junior lab");
-
         courses.add(math);
         courses.add(english);
         courses.add(cs);
 
         courses.get(1).setCourseAvailability(true);
 
-        newUser= new Professor("Tyler",courses,"P");
-        super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_prof_main);
+        newUser= new Professor("Tyler",courses,"P");
+        mProfLabel.setText("Professor User"); //TODO load professor name here
+
         makeButtons();
     }
+
     public void openPinWindow(View view)
     {
         Intent inten= new Intent(this,SignInMapsActivity.class);
@@ -52,32 +61,43 @@ ProfMain extends AppCompatActivity
 
     public void makeButtons()
     {
-        final Intent inten= new Intent(this,SignInPin.class);
-
+        LinearLayout mParentLayout = (LinearLayout) findViewById(R.id.prof_linear);
 
         for(int i=0;i<courses.size();i++)
         {
-            TableRow row = new TableRow(this);
-            TableLayout tableLayout = (TableLayout)findViewById(R.id.TableLayout);
+            LinearLayout dualView = new LinearLayout(getApplicationContext());
+            mParentLayout.addView(dualView);
+            dualView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            dualView.setOrientation(LinearLayout.HORIZONTAL);
+
             Button btn= new Button(this);
+            LinearLayout.LayoutParams buttonParams =
+                    new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.FILL_PARENT);
+
+            buttonParams.weight = 1;
+            btn.setLayoutParams(buttonParams);
+            buttonParams.setMargins(0,60,0,0);
+            btn.setId(i);
+
             btn.setText(newUser.getClassName(courses.get(i)));
-            btn.setHeight(150);
-            btn.setWidth(100);
+
+            dualView.addView(btn);
+
             btn.setOnClickListener(new View.OnClickListener()
             {
                 @Override
                 public void onClick(View view)
-            {
-                openPinWindow(view);
-            }
-
+                {
+                    openPinWindow(view);
+                }
             });
-            tableLayout.addView(row);
-            row.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT,TableLayout.LayoutParams.MATCH_PARENT,1.0f));
-            row.addView(btn);
+
             if (courses.get(i).getCourseAvailibility() == true)
             {
-                btn.setBackgroundColor(Color.GREEN);
+                btn.setBackgroundColor(Color.rgb(50, 205, 50)); //GREEN
+            }else
+            {
+                btn.setBackgroundColor(Color.rgb(220, 220 ,220)); //GRAY
             }
         }
     }
