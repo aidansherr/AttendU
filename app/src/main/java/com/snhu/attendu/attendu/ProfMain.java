3e36903396398b2a47c2dbbf58b22bb860ea6306
Travis Guthrie <tjg0512@gmail.com>
2018-03-27 11:07:11 -0400

    Reverted changes and re-pushed to fix master

--- app/src/main/java/com/snhu/attendu/attendu/ProfMain.java
+++ app/src/main/java/com/snhu/attendu/attendu/ProfMain.java
@@ -11,8 +11,6 @@ import android.widget.TableLayout;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.TableRow;
 import android.widget.TextView;
-<<<<<<< HEAD
-=======
 
 import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.database.DataSnapshot;
@@ -21,7 +19,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
->>>>>>> 927bb31cfb81a84bdcf79d9c60979e8d67e8c859
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,56 +29,27 @@ import static android.R.interpolator.linear;
 public class
 ProfMain extends AppCompatActivity
 {
-<<<<<<< HEAD
-    List<Course> courses= new ArrayList<Course>();
-=======
 
 
     private View mLayout;
     private TextView mProfLabel;
 
 
->>>>>>> 927bb31cfb81a84bdcf79d9c60979e8d67e8c859
     Professor newUser;
     @Override
     protected void onCreate(Bundle savedInstanceState)
 
     {
-<<<<<<< HEAD
+        FirebaseDatabase mDatabase;
+        mDatabase= FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference=FirebaseDatabase.getInstance().getReference();
 
-<<<<<<< HEAD
 
-        Course math=new Course("Math");
-        Course english=new Course("English");
-        Course cs= new Course("Junior lab");
-=======
->>>>>>> parent of 50dc79d... Merge remote-tracking branch 'origin/DatabaseWork'
 
-        courses.add(math);
-        courses.add(english);
-        courses.add(cs);
-
-<<<<<<< HEAD
-        courses.get(1).setCourseAvailability(true);
-
-<<<<<<< HEAD
-=======
-        Course math=new Course("Math");
-        Course english=new Course("English");
-        Course cs= new Course("Junior lab");
-
-        courses.add(math);
-        courses.add(english);
-        courses.add(cs);
->>>>>>> parent of 50dc79d... Merge remote-tracking branch 'origin/DatabaseWork'
-=======
         Intent i= getIntent();
         newUser= (Professor) i.getSerializableExtra("Professor");
->>>>>>> 927bb31cfb81a84bdcf79d9c60979e8d67e8c859
 
-        courses.get(1).setCourseAvailability(true);
 
-<<<<<<< HEAD
 //This pulls the professor object from the database and uses that object to load buttons based on the Professors courses
         databaseReference.child("Professor").child("Professor_ID").addValueEventListener(new ValueEventListener() {
             @Override
@@ -101,19 +69,6 @@ ProfMain extends AppCompatActivity
             }
         });
 
-<<<<<<< HEAD
-        
-=======
-        newUser= new Professor("Tyler",courses,"P");
->>>>>>> parent of 50dc79d... Merge remote-tracking branch 'origin/DatabaseWork'
-=======
-        newUser= new Professor("Tyler",courses,"P");
->>>>>>> parent of 50dc79d... Merge remote-tracking branch 'origin/DatabaseWork'
-        super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_prof_main);
-        makeButtons();
-=======
 
 
         super.onCreate(savedInstanceState);
@@ -124,7 +79,6 @@ ProfMain extends AppCompatActivity
 
 
 
->>>>>>> 927bb31cfb81a84bdcf79d9c60979e8d67e8c859
     }
     public void openPinWindow(View view)
     {
@@ -132,17 +86,9 @@ ProfMain extends AppCompatActivity
         startActivity(inten);
     }
 
-    public void makeButtons()
+    public void makeButtons(Professor newUser)
     {
         final Intent inten= new Intent(this,SignInPin.class);
-<<<<<<< HEAD
-
-
-        for(int i=0;i<courses.size();i++)
-        {
-            TableRow row = new TableRow(this);
-            TableLayout tableLayout = (TableLayout)findViewById(R.id.TableLayout);
-=======
         LinearLayout mParentLayout = (LinearLayout) findViewById(R.id.prof_linear);
         List<Course> profList= newUser.getClassList();
 
@@ -153,9 +99,8 @@ ProfMain extends AppCompatActivity
             dualView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
             dualView.setOrientation(LinearLayout.HORIZONTAL);
 
->>>>>>> 927bb31cfb81a84bdcf79d9c60979e8d67e8c859
             Button btn= new Button(this);
-            btn.setText(newUser.getClassName(courses.get(i)));
+            btn.setText(newUser.getClassName(profList.get(i)));
             btn.setHeight(150);
             btn.setWidth(100);
 
@@ -180,12 +125,6 @@ ProfMain extends AppCompatActivity
                     openPinWindow(view);
                 }
             });
-<<<<<<< HEAD
-            tableLayout.addView(row);
-            row.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT,TableLayout.LayoutParams.MATCH_PARENT,1.0f));
-            row.addView(btn);
-            if (courses.get(i).getCourseAvailibility() == true)
-=======
 
 
 
@@ -194,16 +133,10 @@ ProfMain extends AppCompatActivity
 
             if (profList.get(i).getCourseAvailibility() == true)
 
-=======
->>>>>>> parent of 4f4bb80... Fixed the database retreival, and merged with master
             {
                 btn.setBackgroundColor(Color.rgb(50, 205, 50)); //GREEN
             }
-<<<<<<< HEAD
             else
->>>>>>> 927bb31cfb81a84bdcf79d9c60979e8d67e8c859
-=======
->>>>>>> parent of 4f4bb80... Fixed the database retreival, and merged with master
             {
                 btn.setBackgroundColor(Color.rgb(220, 220 ,220)); //GRAY
             }
